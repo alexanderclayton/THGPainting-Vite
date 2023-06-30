e5d51538faaec48efd2465ecec00b8820eb5c273
Alex Clayton <alexander.clayton.email@gmail.com>
2023-06-30 13:30:53 -0500

    responsive navbar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,56 @@
-import Navbar from "./Navbar";
+import React, { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+import authServiceInstance from '../utils/auth';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import logo from '../assets/react.svg';
 
 const Header = () => {
-    return (
-        <div>
-            <Navbar />
-        </div>
-    )
+  const navRef = useRef();
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
+  const closeNavbar = () => {
+    setIsNavbarOpen(false);
+  };
+
+  const handleLogout = () => {
+    userLogout();
+    window.location.href = '/';
+  };
+
+  const userLogout = () => {
+    authServiceInstance.logout();
+    window.location.reload();
+  };
+
+  return (
+    <header className="header">
+      <img src={logo} alt="thg logo filler" />
+      <nav ref={navRef} className={`navbar ${isNavbarOpen ? 'display-navbar' : ''}`}>
+        <ul className="navbar-list">
+          <li><Link className="navbar-link" to="/" onClick={closeNavbar}>Home</Link></li>
+          <li><Link className="navbar-link" to="/about" onClick={closeNavbar}>About Us</Link></li>
+          <li><Link className="navbar-link" to="/why-thg" onClick={closeNavbar}>Why THG?</Link></li>
+          <li><Link className="navbar-link" to="/gallery" onClick={closeNavbar}>Gallery</Link></li>
+          <li><Link className="navbar-link" to="/contact" onClick={closeNavbar}>Contact Us</Link></li>
+
+          {!authServiceInstance.loggedIn() && (
+            <li><Link className="btn login-btn" to="/login" onClick={closeNavbar}>Login</Link></li>
+          )}
+
+          {authServiceInstance.loggedIn() && (
+            <li><button className="btn login-btn" onClick={handleLogout}>Logout</button></li>
+          )}
+        </ul>
+      </nav>
+      <button className="btn menu-btn" onClick={toggleNavbar}>
+        {isNavbarOpen ? <AiOutlineClose style={{ color: 'var(--gold)' }}/> : <AiOutlineMenu style={{ color: 'var(--gold)' }}/>}
+      </button>
+    </header>
+  );
 };
 
 export default Header;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import authServiceInstance from '../utils/auth';
+import { AiOutlineMenu } from 'react-icons/ai'
 import logo from '../assets/react.svg';
 
 const Navbar = () => {
+    const navRef = useRef();
+
+    const displayNavbar = () => {
+        navRef.current.classList.toggle("display-navbar")
+    }
+
     const handleLogout = () => {
         userLogout();
         window.location.href = '/';
@@ -15,22 +22,27 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="navbar">
+        <div className="header">
             <img src={logo} alt="thg logo filler" />
-            <ul className="navbar-list">
-                <li><Link className="navbar-link" to="/">Home</Link></li>
-                <li><Link className="navbar-link" to="/about">About Us</Link></li>
-                <li><Link className="navbar-link" to="/why-thg">Why THG?</Link></li>
-                <li><Link className="navbar-link" to="/gallery">Gallery</Link></li>
-                <li><Link className="navbar-link" to="/contact">Contact Us</Link></li>
+            <nav ref={navRef} className="navbar">
+                <ul className="navbar-list">
+                    <li><Link className="navbar-link" to="/">Home</Link></li>
+                    <li><Link className="navbar-link" to="/about">About Us</Link></li>
+                    <li><Link className="navbar-link" to="/why-thg">Why THG?</Link></li>
+                    <li><Link className="navbar-link" to="/gallery">Gallery</Link></li>
+                    <li><Link className="navbar-link" to="/contact">Contact Us</Link></li>
 
-                {authServiceInstance.loggedIn() || <li><Link className="btn login-btn" to="/login">Login</Link></li>}
+                    {authServiceInstance.loggedIn() || <li><Link className="btn login-btn" to="/login">Login</Link></li>}
 
-                {authServiceInstance.loggedIn() && (
-                    <li><button className="btn login-btn" onClick={handleLogout}>Logout</button></li>
-                )}
-            </ul>
-        </nav>
+                    {authServiceInstance.loggedIn() && (
+                        <li><button className="btn login-btn" onClick={handleLogout}>Logout</button></li>
+                    )}
+                </ul>
+            </nav>
+            <button className="menu-btn" onClick={displayNavbar}>
+                <AiOutlineMenu />
+            </button>
+        </div>
     )
 };
 
